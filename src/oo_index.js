5cf343071dc13527da849fa31409b58a8240a5e7
Brennan Blair <jbrennan.blair@gmail.com>
2018-10-31 17:39:46 -0400

    Done

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,30 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log('DOM HAS LOADED')
+
+  const newTaskForm = document.getElementById('create-task-form')
+  const divForAppendingNewTasks = document.querySelector("#tasks")
+
+    newTaskForm.addEventListener("submit", function(event) {
+      event.preventDefault()
+      const newTaskInputField = event.target.querySelector("#new-task-description")
+      const newTaskString = newTaskInputField.value
+
+  const newTaskLITag = document.createElement("li")
+  const deleteTaskButton = document.createElement("BUTTON")
+
+  newTaskLITag.innerText = newTaskString
+  deleteTaskButton.innerText = 'x'
+
+  divForAppendingNewTasks.appendChild(newTaskLITag)
+  newTaskLITag.appendChild(deleteTaskButton)
+
+  divForAppendingNewTasks.addEventListener('click', function(event) {
+    debugger
+    event.target.parentElement.remove()
+    });
+
+
+    event.target.reset()
+  });
 })
