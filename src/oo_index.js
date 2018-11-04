7d1e1fb78a47ffddf3435084d2540f5fa8e9f095
asc5025 <albert.s.chun@gmail.com>
2018-11-04 14:38:36 -0500

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newTaskInput = document.querySelector('#new-task-description')
+  const newTaskForm = document.querySelector('#create-task-form')
+  const tasksList = document.querySelector('#tasks')
+  console.log(tasksList)
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log('DOM HAS LOADED')
-})
+  newTaskForm.addEventListener('submit', function formSubmitHandler(event) {
+    event.preventDefault()
+
+    const newCommentInputField = event.target.querySelector('#new-task-description')
+    const newTaskString = newCommentInputField.value
+    const newTaskLiTag = document.createElement('li')
+
+    newTaskLiTag.innerText = newTaskString
+    const appendedLi = tasksList.appendChild(newTaskLiTag)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'x'
+
+    newTaskLiTag.appendChild(deleteButton)
+
+    deleteButton.addEventListener('click', function () {
+      appendedLi.remove()
+
+    })
+
+    event.target.reset()
+  })
+
+
+
+
+}) //end of DOMContentLoaded
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// console.log('DOM HAS LOADED')
+//
+// const input = document.getElementById('new-task-description')
+// const submissionForm = document.getElementById('create-task-form')
+// const list = document.getElementById('tasks')
+//
+// submissionForm.addEventListener('submit', function(event) {
+//  event.preventDefault()
+//  console.log(event.target)
+//
+//  const newCommentInputField = event.target.querySelector('#new-task-description')
+//  const newTaskString = newCommentInputField.value
+//  const newTaskLiTag = document.createElement('li')
+//
+//  newTaskLiTag.innerText = newTaskString
+//
+//  list.appendChild(newTaskLiTag)
+//
+//  event.target.reset()
+// })
+// })
